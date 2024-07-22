eb03f7f25f0592487ef239c3826e1b7e37023845
Anton Stafeev <stafeevt@mail.ru>
2024-07-22 15:28:02 +0500

    start project

---
+++ WeatherVision/Supporting Files/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  WeatherVision
+//
+//  Created by Антон Стафеев on 22.07.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.makeKeyAndVisible()
+        window.rootViewController = ViewController()
+        self.window = window
+        return true
+    }
+}
+
