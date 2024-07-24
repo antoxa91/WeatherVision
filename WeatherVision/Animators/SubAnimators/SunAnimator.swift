ecb0280ec36f57181dc737f882a9eba8df631d5e
Anton Stafeev <stafeevt@mail.ru>
2024-07-24 05:18:13 +0500

    remade

--- WeatherVision/Animators/SubAnimators/SunAnimator.swift
+++ WeatherVision/Animators/SubAnimators/SunAnimator.swift
@@ -7,45 +7,95 @@
 
 import UIKit
 
-final class SunAnimator: BaseAnimator {
-    init() {
-        super.init(emitterSize: CGSize(width: 1, height: 1),
-                   emitterPosition: CGPoint(x: UIScreen.main.bounds.width, y: 0))
+
+final class SunAnimator {
+    private var sunView: UIView?
+    private var view: UIView?
+    private let sunSize: CGFloat = 100
+    private let rayCount = 50
+    private let rayLength: CGFloat = .random(in: 90...100)
+    
+    private func configure() {
+        guard let view else { return }
+        let sunFrame = CGRect(x: -sunSize, y: (view.bounds.height - sunSize) / 2,
+                              width: sunSize, height: sunSize)
+        let sunView = UIView(frame: sunFrame)
+        sunView.layer.cornerRadius = sunSize / 2
+        view.addSubview(sunView)
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = sunView.bounds
+        gradientLayer.cornerRadius = sunSize / 2
+        gradientLayer.colors = [UIColor.systemYellow.cgColor, UIColor.yellow.cgColor, UIColor.systemYellow.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        sunView.layer.insertSublayer(gradientLayer, at: 0)
+        
+        self.sunView = sunView
+        
+        createRays()
     }
     
-    override func configure() -> CAEmitterLayer {
-        let layer = super.configure()
-        layer.emitterShape = .point
-        return layer
+    private func createRays() {
+        guard let sunView = sunView else { return }
+        
+        for i in 0..<rayCount {
+            let angle = CGFloat(i) * (2.0 * .pi / CGFloat(rayCount))
+            let rayPath = UIBezierPath()
+            rayPath.move(to: CGPoint(x: sunSize / 2, y: sunSize / 2))
+            rayPath.addLine(to: CGPoint(x: sunSize / 2 + cos(angle) * rayLength,
+                                        y: sunSize / 2  + sin(angle) * rayLength))
+            
+            let rayLayer = CAShapeLayer()
+            rayLayer.path = rayPath.cgPath
+            rayLayer.lineWidth = 2.0
+            rayLayer.strokeColor = UIColor.yellow.cgColor
+            rayLayer.fillColor = UIColor.clear.cgColor
+            rayLayer.lineCap = .round
+            
+            sunView.layer.addSublayer(rayLayer)
+        }
     }
     
-    override func createEmitterCell() -> CAEmitterCell {
-        let cell = CAEmitterCell()
-        cell.contents = UIImage(resource: .sunLights).cgImage
-        cell.birthRate = 1
-        cell.lifetime = 10
-        cell.velocity = 15
-        cell.emissionLongitude = -.pi / 2
-        cell.emissionRange = CGFloat.pi / 4
-        cell.zAcceleration = 9.8
-        cell.scale = 0.3
-        cell.spin = 0.1
-        cell.spinRange = 0.2
-        return cell
+    private func createRotation() {
+        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotationAnimation.fromValue = 0.0
+        rotationAnimation.toValue = CGFloat.pi * 2.0
+        rotationAnimation.duration = 30.0
+        rotationAnimation.repeatCount = .infinity
+        self.sunView?.layer.add(rotationAnimation, forKey: "rotationAnimation")
+    }
+    
+    private func createPulse() {
+        let pulseAnimation = CABasicAnimation(keyPath: "transform.scale")
+        pulseAnimation.fromValue = 1.0
+        pulseAnimation.toValue = 1.1
+        pulseAnimation.duration = 1.0
+        pulseAnimation.autoreverses = true
+        pulseAnimation.repeatCount = .infinity
+        self.sunView?.layer.add(pulseAnimation, forKey: "pulseAnimation")
     }
 }
 
-//MARK: - WeatherAnimationControllerProtocol
+// MARK: WeatherAnimatorProtocol
 extension SunAnimator: WeatherAnimatorProtocol {
     func startAnimating(view: UIView) {
-        stopAnimating()
-        let layer = configure()
-        view.layer.addSublayer(layer)
-        self.emitterLayer = layer
+        self.view = view
+        
+        configure()
+        createPulse()
+        createRotation()
+
+        // Анимация появления солнца
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: .curveEaseOut, animations: {
+            self.sunView?.frame.origin.x = view.bounds.width - self.sunSize - 30
+            self.sunView?.frame.origin.y = view.bounds.height / 5
+        })
     }
     
     func stopAnimating() {
-        emitterLayer?.removeFromSuperlayer()
-        emitterLayer = nil
+        sunView?.layer.removeAllAnimations()
+        sunView?.removeFromSuperview()
+        sunView = nil
     }
 }
